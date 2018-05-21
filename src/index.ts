a773ce11bc45890720b24fe432403dbee6463e7c
Joseph Cheung <josephktcheung@gmail.com>
2018-05-21 16:28:13 +0800

    Run apollo fetch to demonstrate batch request failure

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
-import {bootstrap} from "vesper";
-import {PostController} from "./controller/PostController";
-import {Post} from "./entity/Post";
-import {CategoryController} from "./controller/CategoryController";
-import {Category} from "./entity/Category";
-import {PostResolver} from "./resolver/PostResolver";
+import { bootstrap } from "vesper";
+import { PostController } from "./controller/PostController";
+import { Post } from "./entity/Post";
+import { CategoryController } from "./controller/CategoryController";
+import { Category } from "./entity/Category";
+import { PostResolver } from "./resolver/PostResolver";
+import { createApolloFetch } from 'apollo-fetch';
+
+const apolloFetch = createApolloFetch({ uri: 'http://localhost:3000/graphql' });
+const request = {
+    "operationName": "CreateCategory",
+    "query": "mutation CreateCategory {\n  categorySave(name: \"a\") {name}}\n"
+};
+
+const run = async () => {
+    const single = await apolloFetch(request);
+    console.log('signle request reesult: ', JSON.stringify(single, null, 2));
+    const multiple = await apolloFetch([request, request]);
+    console.log('multiple requests result: ', JSON.stringify(multiple, null, 2));
+}
 
 bootstrap({
     port: 3000,
@@ -22,7 +36,9 @@ bootstrap({
 }).then(() => {
     console.log("Your app is up and running on http://localhost:3000 . " +
         "You can use Playground in development mode on http://localhost:3000/playground");
-
+    return run();
 }).catch(error => {
     console.error(error.stack ? error.stack : error);
 });
+
+
