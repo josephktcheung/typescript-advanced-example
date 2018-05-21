91ccd7bad62d87ab8c1d3b47a446814f413ddbf1
Joseph Cheung <josephktcheung@gmail.com>
2018-05-21 16:42:29 +0800

    Switch to pg to see different result

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,26 @@ import { CategoryController } from "./controller/CategoryController";
 import { Category } from "./entity/Category";
 import { PostResolver } from "./resolver/PostResolver";
 import { createApolloFetch } from 'apollo-fetch';
+import { getConnection } from "typeorm";
 
 const apolloFetch = createApolloFetch({ uri: 'http://localhost:3000/graphql' });
-const request = {
+const generateRequest = name => ({
     "operationName": "CreateCategory",
-    "query": "mutation CreateCategory {\n  categorySave(name: \"a\") {name}}\n"
-};
+    "query": `mutation CreateCategory {\n  categorySave(name: \"${name}\") {name}}\n`
+});
 
 const run = async () => {
-    const single = await apolloFetch(request);
-    console.log('signle request reesult: ', JSON.stringify(single, null, 2));
-    const multiple = await apolloFetch([request, request]);
+    const single = await apolloFetch(generateRequest('single'));
+    console.log('single request result: ', JSON.stringify(single, null, 2));
+    const multiple = await apolloFetch([generateRequest('multiple 1'), generateRequest('multiple 2')]);
     console.log('multiple requests result: ', JSON.stringify(multiple, null, 2));
+}
+
+const checkPersistence = async () => {
+    const categoryRepository = getConnection().manager.getRepository(Category);
+    const categories = await categoryRepository.find();
+
+    console.log('categories persisted in db: ', JSON.stringify(categories, null, 2));
 }
 
 bootstrap({
@@ -37,8 +45,9 @@ bootstrap({
     console.log("Your app is up and running on http://localhost:3000 . " +
         "You can use Playground in development mode on http://localhost:3000/playground");
     return run();
-}).catch(error => {
-    console.error(error.stack ? error.stack : error);
-});
-
-
+}).then(() => {
+    return checkPersistence();
+})
+    .catch(error => {
+        console.error(error.stack ? error.stack : error);
+    });
